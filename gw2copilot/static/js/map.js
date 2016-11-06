c0e822ec913a0e4ace505d886ceaa87b1d567b12
Jason Antman <jason@jasonantman.com>
2016-11-05 21:40:13 -0400

    trying to get leaflet to work

--- gw2copilot/static/js/map.js
+++ gw2copilot/static/js/map.js
@@ -1,5 +1,5 @@
 /*
-gw2copilot/css/main.css
+gw2copilot/js/map.js
 
 The latest version of this package is available at:
 <https://github.com/jantman/gw2copilot>
@@ -37,10 +37,61 @@ Jason Antman <jason@jasonantman.com> <http://www.jasonantman.com>
 ################################################################################
 */
 
-/* ==========================================================================
-   Author's custom styles
-   ========================================================================== */
+var map;
 
-#mapcontainer {
-    height: 100%;
+function unproject(coord) {
+    return map.unproject(coord, map.getMaxZoom());
 }
+
+function onMapClick(e) {
+    console.log("You clicked the map at " + map.project(e.latlng));
+}
+
+$(document).ready(function () {
+    "use strict";
+
+    var southWest, northEast;
+
+    map = L.map("mapcontainer", {
+        minZoom: 0,
+        maxZoom: 7,
+        crs: L.CRS.Simple
+    }).setView([0, 0], 0);
+
+    southWest = unproject([0, 32768]);
+    northEast = unproject([32768, 0]);
+
+    map.setMaxBounds(new L.LatLngBounds(southWest, northEast));
+
+    L.tileLayer("https://tiles.guildwars2.com/1/1/{z}/{x}/{y}.jpg", {
+        minZoom: 0,
+        maxZoom: 7,
+        continuousWorld: true
+    }).addTo(map);
+
+    map.on("click", onMapClick);
+
+    $.getJSON("https://api.guildwars2.com/v1/map_floor.json?continent_id=1&floor=1", function (data) {
+        var region, gameMap, i, il, poi;
+
+        for (region in data.regions) {
+            region = data.regions[region];
+
+            for (gameMap in region.maps) {
+                gameMap = region.maps[gameMap];
+
+                for (i = 0, il = gameMap.points_of_interest.length; i < il; i++) {
+                    poi = gameMap.points_of_interest[i];
+
+                    if (poi.type != "waypoint") {
+                        continue;
+                    }
+
+                    L.marker(unproject(poi.coord), {
+                        title: poi.name
+                    }).addTo(map);
+                }
+            }
+        }
+    });
+});
