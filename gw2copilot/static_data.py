7f86fa8dbbb29ed0fb5f0e1e804619e7ba24e6b9
Jason Antman <jason@jasonantman.com>
2016-11-08 17:05:19 -0500

    issue #9 issue #16 - maintain a static list of 'world' zones since the API doesn't seem to have this data

---
+++ gw2copilot/static_data.py
@@ -0,0 +1,85 @@
+"""
+gw2copilot/static_data.py
+
+This file maintains bits of data that need to be manually-curated.
+
+The latest version of this package is available at:
+<https://github.com/jantman/gw2copilot>
+
+################################################################################
+Copyright 2016 Jason Antman <jason@jasonantman.com> <http://www.jasonantman.com>
+
+    This file is part of gw2copilot.
+
+    gw2copilot is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    gw2copilot is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with gw2copilot.  If not, see <http://www.gnu.org/licenses/>.
+
+The Copyright and Authors attributions contained herein may not be removed or
+otherwise altered, except to add the Author attribution of a contributor to
+this work. (Additional Terms pursuant to Section 7b of the AGPL v3)
+################################################################################
+While not legally required, I sincerely request that anyone who finds
+bugs please submit them at <https://github.com/jantman/gw2copilot> or
+to me via email, and that you send any contributions or improvements
+either as a pull request on GitHub, or to me via email.
+################################################################################
+
+AUTHORS:
+Jason Antman <jason@jasonantman.com> <http://www.jasonantman.com>
+################################################################################
+"""
+
+#: Dict mapping map_id to map_name for "normal" world zones that appear on the
+#: standard world map.
+world_zones = {
+    15: "Queensdale",
+    17: "Harathi Hinterlands",
+    18: "Divinity's Reach",
+    19: "Plains of Ashford",
+    20: "Blazeridge Steppes",
+    21: "Fields of Ruin",
+    22: "Fireheart Rise",
+    23: "Kessex Hills",
+    24: "Gendarran Fields",
+    25: "Iron Marches",
+    26: "Dredgehaunt Cliffs",
+    27: "Lornar's Pass",
+    28: "Wayfarer Foothills",
+    29: "Timberline Falls",
+    30: "Frostgorge Sound",
+    31: "Snowden Drifts",
+    32: "Diessa Plateau",
+    34: "Caledon Forest",
+    35: "Metrica Province",
+    39: "Mount Maelstrom",
+    50: "Lion's Arch",
+    51: "Straits of Devastation",
+    53: "Sparkfly Fen",
+    54: "Brisban Wildlands",
+    62: "Cursed Shore",
+    65: "Malchor's Leap",
+    73: "Bloodtide Coast",
+    91: "The Grove",
+    139: "Rata Sum",
+    218: "Black Citadel",
+    326: "Hoelbrak",
+    873: "Southsun Cove",
+    988: "Dry Top",
+    1015: "The Silverwastes",
+    1041: "Dragon's Stand",
+    1043: "Auric Basin",
+    1045: "Tangled Depths",
+    1052: "Verdant Brink",
+    1165: "Bloodstone Fen",
+    1175: "Ember Bay"
+}
