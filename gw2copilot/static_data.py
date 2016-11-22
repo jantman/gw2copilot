5130ffc1d06e4df0be22929f6e5ed8d446943068
Jason Antman <jason@jasonantman.com>
2016-11-22 06:10:09 -0500

    issue #30 - start adding banker and trading post locations, but these need to be manually located... giving up on that for now

--- gw2copilot/static_data.py
+++ gw2copilot/static_data.py
@@ -83,3 +83,63 @@ world_zones = {
     1165: "Bloodstone Fen",
     1175: "Ember Bay"
 }
+
+#: List of Black Lion Trading Posts; each key is an identifier and each value
+#: is a coordinate [x, y] list
+#: see https://wiki.guildwars2.com/wiki/Trading_Post
+trading_posts = {
+    'Black Citadel - Memorial Quadrant': [],
+    'Divinity\'s Reach - Dwayna High Road': [],
+    'Hoelbrak - Trade Commons': [],
+    'Lion\'s Arch - Trader\'s Forum': [16103.117606165886,14784.75094229819],
+    'Rata Sum - Interdisciplinary Accessium': [],
+    'The Grove - Reckoner\'s Terrace': [10432.584755667544,20937.22088452924],
+    'Ascalon': [],
+    'Diessa Plateau - Town of Nolan': [],
+    'Fields of Ruin - Stronghold of Ebonhawke': [],
+    'Kryta': [],
+    'Queensdale - Altar Brook Trading Post - Jaklan': [],
+    'Kessex Hills - Delanian Foothills': [],
+    'Gendarran Fields - Traveler\'s Dale - Grietje': [],
+    'Harathi Hinterlands - Cloven Hoof Pass - Seraph Sergeant': [],
+    'Harathi Hinterlands - Arca Lake - Jadus Phinn': [],
+    'Harathi Hinterlands - Nightguard Beach - Debbie Lantys': [],
+    'Bloodtide Coast - Portage Hills': [],
+    'Maguuma Jungle': [],
+    'Mount Maelstrom - Sootberme': [],
+    'Ruins of Orr': [],
+    'Malchor\'s Leap - Pagga\'s Post': [],
+    'Cursed Shore - Pursuit Pass - Elana': [],
+    'Shiverpeak Mountains': [],
+    'Snowden Drifts - Hunter\'s Pitfalls - Trader Jan and Trader Jed': [],
+    'Lornar\'s Pass - Durmand Priory': [],
+    'Timberline Falls - Kyesjard': [],
+    'Frostgorge Sound - Yakkington\'s Toil': []
+}
+
+#: List of Bankers; each key is an identifier and each value
+#: is a coordinate [x, y] list
+#: see https://wiki.guildwars2.com/wiki/Banker
+bankers = {
+    '"Wheezy" Teicheria': [],  # Fort Marriner (Lion's Arch)
+    'Argeid': [10460.23631961077,21112.515109055323],  # Reckoner's Terrace (The Grove)
+    'Banker': [16328.441601395718,14586.077972198487],  # Trader's Forum (Lion's Arch)
+    'Banker2': [16342.52235058628,14590.250712594605],  # Trader's Forum (Lion's Arch)
+    'Banker Blossri': [],  # Sootberme (Mount Maelstrom)
+    'Consortium Banker': [],  # Southsun Shoals (Southsun Cove)
+    'Cratus': [],  # Canton Factorium (Black Citadel)
+    'Cuina': [],  # Canal Ward (Lion's Arch)
+    'Drifa': [],  # Canal Ward (Lion's Arch)
+    'Froda': [],  # Hall of Memories (Heart of the Mists)
+    'Ikka': [],  # Interdisciplinary Accessium (Rata Sum)
+    'Kallin': [],  # Trade Commons (Hoelbrak)
+    'Lizette': [],  # Canal Ward (Lion's Arch)
+    'Mona': [10471.991963881128,21114.38070884897],  # Reckoner's Terrace (The Grove)
+    'Orres': [],  # Canton Factorium (Black Citadel)
+    'Randver': [],  # Trade Commons (Hoelbrak)
+    'Salanda': [],  # Dwayna High Road (Divinity's Reach)
+    'Tekk': [],  # Interdisciplinary Accessium (Rata Sum)
+    'Thurmain': [],  # Dwayna High Road (Divinity's Reach)
+    'Ubbi Skarsgard': [],  # Stronghold of Ebonhawke (Fields of Ruin)
+    'Vaness': [],  # Dwayna High Road (Divinity's Reach)
+}
